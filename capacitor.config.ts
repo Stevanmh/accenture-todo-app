0965de74fe3955f1e08c05de3ccb9d28d0498aff
Stevanmh <esteban.murillo0105@gmail.com>
2026-08-09 19:07:30 -0500

    config: update app bundle ID and name to production values

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'accenture-todo-app',
+  appId: 'com.esteban.accenture.todoapp',
+  appName: 'Accenture Todo App',
   webDir: 'www'
 };
 
